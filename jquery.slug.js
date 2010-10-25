f5616b84c1e5d77c9dd164f3611e773fb5da8072
Atratus <atratus@go2.pl>
2010-10-25 15:38:14 +0200

    added usage example

--- jquery.slug.js
+++ jquery.slug.js
@@ -1,8 +1,13 @@
-//	jQuery Slug Generation Plugin by Atratus (http://github.com/Atratus/jQuery-Slug-Generation-Plugin)
+// jQuery Slug Generation Plugin by Atratus (http://github.com/Atratus/jQuery-Slug-Generation-Plugin)
 
 // Improved from jQuery Slug Generation Plugin by Perry Trinier (perrytrinier@gmail.com). Licensed under the GPL: http://www.gnu.org/copyleft/gpl.html
 // Improved from urlify.js from Django framework. BSD License: http://code.djangoproject.com/browser/django/trunk/LICENSE
 
+// Usage
+//$(document).ready(function(){ 
+//	$("#NewsTitleEn").slug({slug:\'NewsUrlEn\'});
+//	$("#NewsTitleFr").slug({slug:\'NewsUrlFr\'});
+//});
 
 jQuery.fn.slug = function(options) {
 	var settings = {
